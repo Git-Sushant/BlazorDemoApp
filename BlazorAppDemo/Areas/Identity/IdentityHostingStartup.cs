93cd9630cd6f0ecd49c0fd955dcafbe884724947
Sushant Patil <sushant.patil@anarsolutions.com>
2021-02-04 12:08:10 +0530

    initial commit

---
+++ BlazorAppDemo/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using BlazorAppDemo.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BlazorAppDemo.Areas.Identity.IdentityHostingStartup))]
+namespace BlazorAppDemo.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<BlazorAppDemoContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("BlazorAppDemoContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<BlazorAppDemoContext>();
+            });
+        }
+    }
+}
